63933e055e6cf955f6634824f6bc4ecc738b2271
shlokabari01 <119592364+shlokabari01@users.noreply.github.com>
2024-07-12 21:59:39 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -16,18 +16,18 @@ def generate_soundex(name):
         return ""
 
     # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
+    soundex = [name[0].upper()]
+    prev_code = get_soundex_code(soundex[0])
 
     for char in name[1:]:
         code = get_soundex_code(char)
         if code != '0' and code != prev_code:
-            soundex += code
+            soundex.append(code)
             prev_code = code
         if len(soundex) == 4:
             break
 
     # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
+    soundex.extend(['0'] * (4 - len(soundex)))
 
-    return soundex
+    return ''.join(soundex)

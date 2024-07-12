8ad2bc404b334ecb6098fd6e32d45a62beab0278
shlokabari01 <119592364+shlokabari01@users.noreply.github.com>
2024-07-12 22:05:17 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -9,8 +9,29 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
-    
+    def test_vowels_ignored(self):
+        self.assertEqual(generate_soundex("AeioU"), "A000")
+
+    def test_double_letters(self):
+        self.assertEqual(generate_soundex("Bobb"), "B100")
+
+    def test_name_with_h_and_w(self):
+        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+
+    def test_name_with_vowels_separating(self):
+        self.assertEqual(generate_soundex("Tymczak"), "T522")
+
+    def test_mixed_case(self):
+        self.assertEqual(generate_soundex("RoBeRt"), "R163")
+
+    def test_short_name(self):
+        self.assertEqual(generate_soundex("Li"), "L000")
+
+    def test_name_with_no_mappable_consonants(self):
+        self.assertEqual(generate_soundex("AEIOU"), "A000")
+
+    def test_name_with_repeated_consonants_separated_by_vowels(self):
+        self.assertEqual(generate_soundex("BaeiouBB"), "B100")
+
 if __name__ == '__main__':
     unittest.main()
-
-

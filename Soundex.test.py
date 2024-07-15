706a140b1247ed51bbfa186cd09b3b355c7be86c
shlokabari01 <119592364+shlokabari01@users.noreply.github.com>
2024-07-15 11:11:19 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,5 +1,5 @@
 import unittest
-from Soundex import generate_soundex
+from Soundex import generate_soundex, encode_name
 
 class TestSoundex(unittest.TestCase):
 
@@ -9,29 +9,17 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
 
-    def test_vowels_ignored(self):
-        self.assertEqual(generate_soundex("AeioU"), "A000")
+    def test_encode_name(self):
+        self.assertEqual(encode_name("Aeiou", "A", 0), "A000")
 
-    def test_double_letters(self):
-        self.assertEqual(generate_soundex("Bobb"), "B100")
+    def test_long_string(self):
+        self.assertEqual(generate_soundex("Jackandsmith"), "J532")
 
-    def test_name_with_h_and_w(self):
-        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+    def test_non_alphabetic_characters(self):
+        self.assertEqual(generate_soundex("1234"), "1000")
 
-    def test_name_with_vowels_separating(self):
-        self.assertEqual(generate_soundex("Tymczak"), "T522")
-
-    def test_mixed_case(self):
-        self.assertEqual(generate_soundex("RoBeRt"), "R163")
-
-    def test_short_name(self):
-        self.assertEqual(generate_soundex("Li"), "L000")
-
-    def test_name_with_no_mappable_consonants(self):
-        self.assertEqual(generate_soundex("AEIOU"), "A000")
-
-    def test_name_with_repeated_consonants_separated_by_vowels(self):
-        self.assertEqual(generate_soundex("BaeiouBB"), "B100")
+    def test_mixed_chars(self):
+        self.assertEqual(generate_soundex("Jackfruit@home"), "J163")
 
 if __name__ == '__main__':
     unittest.main()

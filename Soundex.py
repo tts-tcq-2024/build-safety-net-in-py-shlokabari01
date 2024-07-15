763e2162731ccbf9f70e17ea8118b2d004339a50
shlokabari01 <119592364+shlokabari01@users.noreply.github.com>
2024-07-15 11:09:48 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,38 +1,24 @@
+def get_soundex_code(c):
+    return {'B': '1', 'F': '1', 'P': '1', 'V': '1',
+            'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+            'D': '3', 'T': '3',
+            'L': '4',
+            'M': '5', 'N': '5',
+            'R': '6'}.get(c.upper(), '0')
+
+def process_char(char, soundex, prev_code):
+    code = get_soundex_code(char)
+    if code != '0' and code != prev_code:
+        soundex += code
+        prev_code = code
+    return soundex, prev_code
+
 def generate_soundex(name):
     if not name:
         return ""
-
-    soundex_mapping = {
-        'b': '1', 'f': '1', 'p': '1', 'v': '1',
-        'c': '2', 'g': '2', 'j': '2', 'k': '2', 'q': '2', 's': '2', 'x': '2', 'z': '2',
-        'd': '3', 't': '3',
-        'l': '4',
-        'm': '5', 'n': '5',
-        'r': '6'
-    }
-
-    first_letter = name[0].upper()
-    soundex = first_letter
-
-    count = 0
-    last_digit = ''
-
-    for char in name[1:].lower():
-        if char in "aeiouyhw":
-            last_digit = ''
-            continue
-
-        digit = soundex_mapping.get(char, '')
-
-        if digit == last_digit:
-            continue
-
-        if digit:
-            soundex += digit
-            last_digit = digit
-            count += 1
-
-        if count == 3:
+    soundex, prev_code = name[0].upper(), get_soundex_code(name[0])
+    for char in name[1:]:
+        soundex, prev_code = process_char(char, soundex, prev_code)
+        if len(soundex) == 4:
             break
-
     return soundex.ljust(4, '0')
